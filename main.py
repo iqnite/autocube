7bfb3ab54416c32d79c11a017bc155e2ea8d32a6
iqnite <iqnitetechnologies@gmail.com>
2026-07-23 12:06:34 +0200

    Shuffle cube

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from logic.cube import Algorithm, Cube
 from tools.visualizer import visualize_cube
@@ -7,6 +8,7 @@ if __name__ == "__main__":
     with open("examples/solved.json", "r", encoding="utf-8") as f:
         face_data = json.load(f)["faces"]
     cube = Cube.from_json(face_data)
-    cube.apply_algorithm(Algorithm("R"))
+    shuffle_moves = " ".join(random.choices(["U", "D", "L", "R", "F", "B"], k=20))
+    cube.apply_algorithm(Algorithm(shuffle_moves))
     print(cube.to_json())
     visualize_cube(cube)
