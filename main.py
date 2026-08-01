f833d9b208ef78e3767ab13038ba842112b44ec0
iqnite <iqnitetechnologies@gmail.com>
2026-08-01 23:25:26 +0200

    Add command line switch for live

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from logic.solver import solve_cube
 from tools.visualizer import visualize_cube
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and sys.argv[1] != "--live":
         file_path = sys.argv[1]
         try:
             with open(file_path, "r", encoding="utf-8") as f:
@@ -27,12 +27,15 @@ if __name__ == "__main__":
         shuffle_moves = " ".join(random.choices(["U", "D", "L", "R", "F", "B"], k=40))
         cube.apply_algorithm(Algorithm(shuffle_moves))
         print("No file provided, solving random cube...")
-    # cube.on_move = lambda: visualize_cube(cube, live=True)
+    live = "--live" in sys.argv
+    if live:
+        cube.on_move = lambda: visualize_cube(cube, live=True)
     cube.move_history.clear()
     solve_cube(cube)
     print("Cube solved!")
     history_algorithm = Algorithm(cube.move_history)
     print("Moves:", history_algorithm)
     print("Moves (optimized):", Algorithm.optimize_move_string(str(history_algorithm)))
-    visualize_cube(cube)
+    if not live:
+        visualize_cube(cube)
     plt.show(block=True)
