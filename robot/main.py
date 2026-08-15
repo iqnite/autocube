f54c50e4b7eb0ba73ae8e9a4ced4adca3716f212
iqnite <iqnitetechnologies@gmail.com>
2026-08-15 18:24:35 +0200

    Add server connection

--- robot/main.py
+++ robot/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env pybricks-micropython
-import sys
+
+import socket
 
 from pybricks.ev3devices import ColorSensor, Motor, TouchSensor  # type: ignore
 from pybricks.hubs import EV3Brick  # type: ignore
@@ -8,23 +9,49 @@ from pybricks.parameters import Button, Color, Direction, Port, Stop  # type: ig
 from pybricks.robotics import DriveBase  # type: ignore
 from pybricks.tools import DataLog, StopWatch, wait  # type: ignore
 
-sys.path.append("..")
+HOST = "0.0.0.0"
+PORT = 65432
 
-import logic.mappings
 
-ev3 = EV3Brick()
+class Robot:
+    def __init__(self):
+        self.ev3 = EV3Brick()
+        self.motor_yaw = Motor(Port.A)
+        self.motor_roll = Motor(Port.B)
+        self.motor_scanner = Motor(Port.C)
 
-print(logic.mappings.UP_EDGES)
+    def reset_motor_positions(self):
+        self.motor_yaw.run_until_stalled(-100)
+        self.motor_scanner.run_until_stalled(500)
 
-test_motor = Motor(Port.A)
-test_button = TouchSensor(Port.S3)
-test_light = ColorSensor(Port.S2)
+    def execute_move(self, move):
+        print("Received command for motors:", move)
+
 
 if __name__ == "__main__":
-    test_motor.reset_angle(0)
+    robot = Robot()
+    robot.reset_motor_positions()
 
-    test_motor.run_target(500, 90)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # This setting prevents "Address already in use" errors if you restart the script quickly
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        s.bind((HOST, PORT))
+        s.listen(1)
 
-    while not test_button.pressed():
-        print(test_light.color())
-        wait(100)
+        print("Listening for connection on port", PORT)
+        conn, addr = s.accept()
+        try:
+            print("Connected by laptop at", addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break  # Laptop disconnected
+                command = data.decode("utf-8").strip()
+                robot.execute_move(command)
+                wait(1000)
+                conn.send(b"DONE\n")
+        finally:
+            conn.close()
+    finally:
+        s.close()
