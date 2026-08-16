046d930bc1a0993a5bd01a7e9621315512e3e4fe
iqnite <iqnitetechnologies@gmail.com>
2026-08-16 12:47:00 +0200

    Add alt controller entrypoint

---
+++ autocube_controller.py
@@ -0,0 +1,8 @@
+"""
+Alternative entry point for the controller script.
+"""
+
+from controller import main
+
+if __name__ == "__main__":
+    main.main()
--- controller/main.py
+++ controller/main.py
@@ -9,7 +9,8 @@ from logic.cube import Algorithm
 
 PORT = 65432
 
-if __name__ == "__main__":
+
+def main():
     ev3_ip = sys.argv[1] if len(sys.argv) > 1 else "ev3dev.local"
     with Robot(ev3_ip, PORT) as robot:
         robot.connect()
@@ -22,3 +23,7 @@ if __name__ == "__main__":
             response = robot.apply_motor_algorithm(
                 CubeManipulator().cube_to_motor_algorithm(Algorithm(user_input))
             )
+
+
+if __name__ == "__main__":
+    main()
