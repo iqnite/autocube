b1210ccacf3d676bcc75218cde60875f29e721e8
iqnite <iqnitetechnologies@gmail.com>
2026-08-18 15:22:05 +0200

    Add solve command

--- controller/main.py
+++ controller/main.py
@@ -5,6 +5,7 @@ Main entry point for the robot controller.
 import sys
 
 from controller.connector import CubeManipulator, CubeScanner, Robot
+from logic import solver
 from logic.cube import Algorithm
 from script.visualizer import visualize_cube
 
@@ -17,13 +18,24 @@ def main():
     with Robot(ev3_ip, PORT) as robot:
         robot.connect()
         while True:
-            user_input = input("Enter algorithm, 'cmd', 'scan', 'quit' or 'reset': ")
+            user_input = input("Enter algorithm or command: ")
             if not user_input:
                 continue
             if user_input == "quit":
                 break
             if user_input == "reset":
                 manipulator = CubeManipulator()
+                continue
+            if user_input == "solve":
+                print("Scanning cube...")
+                cube = CubeScanner(robot, manipulator).scan()
+                print("Finding solution...")
+                solution = solver.solve_cube(cube)
+                print("Solving cube...")
+                robot.apply_motor_algorithm(
+                    manipulator.cube_to_motor_algorithm(Algorithm(solution))
+                )
+                print("Cube solved!")
                 continue
             if user_input == "scan":
                 scanner = CubeScanner(robot, manipulator)
